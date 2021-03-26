0ff1a2be994fcb720ec4deaebba9b16db884529a
Gertobin11 <gertobin11@gmail.com>
2021-03-26 14:46:18 +0000

    added the shuffle function ....eventually

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,5 @@
 //I will be using a mix of both javascript and jQuery for this as I feel more competent doing certain things in each
-
-
-
-let matchGame = $("#match-game");
+//document.getElementById("easy").addEventListener("click", shuffle)
 
 let cards = document.getElementsByClassName("match-card");
 
@@ -20,15 +17,14 @@ let lockBoard = false;
 
 //adding a function to turn the cards over
 function turnCard() {
-    if(this === firstCard) return;
-    if(lockBoard) return;
+    if (this === firstCard) return;
+    if (lockBoard) return;
     this.classList.toggle("flip");
-    
-    if(!turnedCard) {
+
+    if (!turnedCard) {
         turnedCard = true;
         firstCard = this;
         selectedCards.push(this);
-        shuffle();
     }
     else {
         lockBoard = true;
@@ -36,13 +32,22 @@ function turnCard() {
         secondCard = this;
         selectedCards.push(this);
     }
-    
-    if(selectedCards.length === 2) {
+
+    if (selectedCards.length === 2) {
         checkForMatch(selectedCards[0], selectedCards[1])
     }
 
 }
-    
+
+// had to modify the shuffle from online tutorial , but the online tutorial wouldnt work so I added a for of loop which did the trick!!! video is here around the 32minute mark https://www.youtube.com/watch?v=ZniVgo8U7ek
+(function shuffle() {
+    let randomPos = Math.floor(Math.random() * 16)
+    for(card of cards) {
+        let randomPos = Math.floor(Math.random() * 16);
+        card.style.order = randomPos;
+    };
+    console.log(randomPos);
+})();
 
 
 //adding an event listen for when the cards are chosen
@@ -52,36 +57,36 @@ for (let i = 0; i < cards.length; i++) {
 
 //going to write a function to check for a match
 function checkForMatch() {
-    if(selectedCards[0].dataset.card === selectedCards[1].dataset.card) {
+    if (selectedCards[0].dataset.card === selectedCards[1].dataset.card) {
         matchedPairs.push(selectedCards[0], selectedCards[1]);
         firstCard.removeEventListener("click", turnCard)
         secondCard.removeEventListener("click", turnCard)
         console.log(matchedPairs)
         selectedCards.length = 0;
         lockBoard = false;
-        
+
 
     }
     //added code for if it doesnt match it will return the cards and empty the selectedCars array, also had to ser a timeout fuction to make it work properly
-    else{
+    else {
         lockBoard = true;
         setTimeout(() => {
-        firstCard.classList.toggle("flip");
-        secondCard.classList.toggle("flip");
-        selectedCards.length = 0;
-        lockBoard = false;
+            firstCard.classList.toggle("flip");
+            secondCard.classList.toggle("flip");
+            selectedCards.length = 0;
+            lockBoard = false;
         }, 1100);
     }
 }
 
-function shuffle() {
-    let cardDeck =[];
-    for (let i = 0; i < cards.length; i++) {
-        cardDeck.push(cards[i]);
-    cardDeck = cardDeck.sort(() => Math.random() - 0.5)
-    }
-    console.log(cardDeck)
-}
+
+    
+
+
+
+
+
+
 
 
 
